00483aa4a100c12df930bb78a26a0a6eeab49bb6
thermionix <thermionix@gmail.com>
2015-08-18 22:06:24 +1000

    new file:   arch-satnav.sh 	new file:   pcduino3-nano-uboot.sh 	new file:   uBoot-pcDuino-Nano/sun7i-a20-pcduino3-nano.dtb 	new file:   uBoot-pcDuino-Nano/u-boot-sunxi-with-spl.bin

---
+++ arch-satnav.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+set -e
+
+if [ $(id -u) = 0 ]; then
+	echo "Don't run as root!"
+	exit 1
+fi
+
+sudo pacman -Sy --noconfirm haveged
+sudo systemctl enable haveged
+sudo systemctl start haveged
+
+if ! command -v pacaur ; then
+	echo "## Installing pacaur AUR Helper"
+
+	sudo pacman -Sy --noconfirm --needed wget base-devel
+
+	if ! grep -q "EDITOR" ~/.bashrc ; then 
+		echo "export EDITOR=\"nano\"" >> ~/.bashrc
+	fi
+
+	#sed -i -e "/^#keyserver-options auto-key-retrieve/s/#//" ~/.gnupg/gpg.conf
+	curl https://aur.archlinux.org/packages/co/cower/cower.tar.gz | tar -zx
+	pushd cower
+	makepkg -s PKGBUILD --install --noconfirm --ignorearch --skippgpcheck
+	popd
+	rm -rf cower
+
+	curl https://aur.archlinux.org/packages/pa/pacaur/pacaur.tar.gz | tar -zx
+	pushd pacaur
+	makepkg -s PKGBUILD --install --noconfirm
+	popd
+	rm -rf pacaur
+fi
+
+echo "Defaults passwd_timeout=0" | sudo tee /etc/sudoers.d/timeout
+echo 'Defaults editor=/usr/bin/nano, !env_editor' | sudo tee /etc/sudoers.d/nano
+
+sudo pacman -S --noconfirm xorg-server xorg-server-utils xorg-xinit mesa
+
+echo "## installing video driver fbdev"
+sudo pacman -S --noconfirm xf86-video-fbdev
+
+echo "## Installing Fonts"
+sudo pacman -S --noconfirm ttf-droid ttf-liberation ttf-dejavu
+
+
+username=`whoami`
+echo "## enabling autologin for user: $username"
+sudo mkdir -p /etc/systemd/system/getty@tty1.service.d
+echo -e "[Service]\nExecStart=\nExecStart=-/usr/bin/agetty --autologin $username --noclear %I 38400 linux" \
+	| sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf
+
+
+
+echo "## enabling X Autostart"
+if ! grep -q "exec startx" ~/.bash_profile ; then 
+	test -f /home/$username/.bash_profile || cp /etc/skel/.bash_profile ~/.bash_profile
+	echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && exec startx" >> ~/.bash_profile
+fi
+
+
+
+cat <<-'EOF' | sudo tee /etc/systemd/system/paccache-clean.timer
+[Unit]
+Description=Clean pacman cache weekly
+
+[Timer]
+OnBootSec=10min
+OnCalendar=weekly
+Persistent=true     
+ 
+[Install]
+WantedBy=timers.target
+EOF
+
+cat <<-'EOF' | sudo tee /etc/systemd/system/paccache-clean.service
+[Unit]
+Description=Clean pacman cache
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/paccache -rk2
+ExecStart=/usr/bin/paccache -ruk0
+EOF
+
+sudo systemctl enable paccache-clean.timer
+
+
+
+sudo pacman -S --noconfirm enlightenment xterm
+echo "exec enlightenment_start" > ~/.xinitrc
+
+
+
+sudo pacman -S --noconfirm gpsd
+
+#sudo pacman -S gpsdrive
+pacaur -S navit
+#pacaur -S roadnav
+#pacaur -S qmapshack
+#pacaur -S foxtrotgps
+#pacaur -S modRana
+
---
+++ pcduino3-nano-uboot.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+git clone git://git.denx.de/u-boot.git
+cd u-boot
+make CROSS_COMPILE=arm-linux-gnueabihf- Linksprite_pcDuino3_Nano_defconfig
+make CROSS_COMPILE=arm-linux-gnueabihf-
+
+mkdir -p ../u-boot-output
+cp u-boot-sunxi-with-spl.bin spl/sunxi-spl.bin u-boot.img ../u-boot-output
+
+cp u-boot.dtb ../u-boot-output/sun7i-a20-pcduino3-nano.dtb
+
+# Zero and Write the new bootloader to the sdcard:
+sudo dd if=/dev/zero of=/dev/sdX bs=1M count=8
+sudo dd if=u-boot-sunxi-with-spl.bin of=/dev/sdX bs=1024 seek=8
+sync
+
+# Also copy sun7i-a20-pcduino3-nano.dtb into /boot/dtbs
+
+
+#  community/uboot-tools 2014.10-1	/usr/bin/mkimage
+
+
+## cat boot.cmd
+#setenv bootargs console=ttyS0 root=/dev/mmcblk0p1 rootwait panic=10 ${extra}
+#ext2load mmc 0 0x43000000 boot/script.bin
+#ext2load mmc 0 0x48000000 boot/uImage
+#bootm 0x48000000
+
+
+# mkimage -C none -A arm -T script -d boot.cmd boot.scr
+
+
+
+# https://github.com/torvalds/linux/blob/master/arch/arm/boot/dts/sun7i-a20-pcduino3-nano.dts
+
+# sun7i-a20-pcduino3-nano.dtb
+#/boot/uImage
+
+# add to /boot/dtbs
+
